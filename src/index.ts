6e48d0e4d210498d3d3c1766b34039efeefc366a
Eduard Deza Castellano <ed@ascetic.io>
2018-03-08 13:23:32 +0100

    Implemented FormModule and export FormServiceConfiguration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+export {FormModule} from './form.module';
 export {FormService} from './form.service';
 export {ControlExclude, ControlReplace, ControlTransform, ControlValidators} from './form.metadata';
+export {FormServiceConfiguration} from './form.service.configuration';
